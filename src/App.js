36be7a852c253440d2e29144b8e077ca6389fc11
EmanuelMiron <emanuel.miron6@gmail.com>
2023-01-17 00:31:39 +0200

    Vid 78 - Created CategoryItem Component and added images to categories

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
-const App = () => {
+import CategoryItem from "./components/category-item/category-item.component";
 
+import "./categories-styles.scss";
+
+const App = () => {
   const categories = [
     {
-      title: 'Hats'
+      id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
-      title: 'Jackets'
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
-      title: 'Sneakers'
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
-      title: 'Womens'
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
-      title: 'Mens'
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
-  ]
+  ];
 
   return (
     <div className="categories-container">
-      {categories.map(({title}) => (
-        <div className="category">
-          <div className="background-image" />
-          <div className="category-body">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
       ))}
     </div>
-  )
-}
+  );
+};
 
 export default App;
