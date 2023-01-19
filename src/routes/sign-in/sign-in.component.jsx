142fee97f8d5c60dd6848b0370e7696a6ab752fb
EmanuelMiron <emanuel.miron6@gmail.com>
2023-01-19 13:56:35 +0200

    Vid 96 - Removed Google Auth with redirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,23 +1,9 @@
-import { useEffect } from "react";
-import { getRedirectResult } from "firebase/auth";
 import {
-  auth,
   signInWithGooglePopup,
   createUserDocumentFromAuth,
-  signInWithGoogleRedirect,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
-  useEffect(() => {
-    const test = async () => {
-        const response = await getRedirectResult(auth);
-        if(response){
-            const userDocRef = await createUserDocumentFromAuth(response.user);
-        }
-    }
-
-    test();
-  }, []);
 
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
@@ -28,9 +14,6 @@ const SignIn = () => {
     <div>
       <h1> Sign In </h1>
       <button onClick={logGoogleUser}>Sign In with Google Popup</button>
-      <button onClick={signInWithGoogleRedirect}>
-        Sign In with Google Redirect
-      </button>
     </div>
   );
 };
